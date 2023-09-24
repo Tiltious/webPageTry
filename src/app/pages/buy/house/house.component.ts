cf0131d89bc930504b93c32c40d0877c47909a88
Tiltious <miltosouzounis91@gmail.com>
2023-09-24 15:00:29 +0300

    com

--- src/app/pages/buy/house/house.component.ts
+++ src/app/pages/buy/house/house.component.ts
@@ -8,17 +8,17 @@ import { HousesService } from '../../shared/houses.service';
   styleUrls: ['./house.component.scss']
 })
 export class HouseComponent implements OnInit {
-  houses:Houses[]=[];
-  houses2:Houses[]=[];
-  constructor(private houserv:HousesService) {
-   }
+  houses: Houses[] = [];
+  houses2: Houses[] = [];
+  constructor(private houserv: HousesService) {
+  }
 
   ngOnInit() {
-    this.houserv.getAllHouses().subscribe((response:any)=>{
-      for(let house of response.data){
+    this.houserv.getAllHouses().subscribe((response: any) => {
+      for (let house of response.data) {
         let mainimg;
-        !!house.mainImageURL?mainimg=house.mainImageURL:mainimg = "assets/missing_photo-medium.jpg";
-        house.buy_or_rent==0&&house.category=='house'?this.houses2.push(new Houses(house.price,house.sq_meters,house.buy_or_rent,house.geography,mainimg)):null
+        !!house.mainImageURL ? mainimg = house.mainImageURL : mainimg = "assets/missing_photo-medium.jpg";
+        house.buy_or_rent == 0 && house.category == 'house' ? this.houses2.push(new Houses(house.price, house.sq_meters, house.buy_or_rent, house.geography, mainimg)) : null
       }
     })
   }
